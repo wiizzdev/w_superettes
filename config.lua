bb9ea8871a5fbe1201e0729d3b59c6bcd3d688c2
wiizz <66723882+wiizzdev@users.noreply.github.com>
2022-06-12 18:26:11 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,48 @@
+Config = {
+    Menu = {
+        Nourriture = {
+            {label = "Pain", name = 'bread', price = 10},
+        },
+        Boissons = {
+            {label = "Eau", name = 'water', price = 5},
+        },
+        Divers = {
+            {label = "Téléphone", name = 'phone', price = 100},
+            {label = "Radio", name = 'radio', price = 50},
+        },
+        Activer = {
+            PaiementBanque = true, -- mettre 'false' pour payer uniquement en liquide
+            Nourriture = true,
+            Boissons = true,
+            Divers = true,
+        },
+    },
+    Position = {
+        {x = 2557.18,  y = 382.4,    z = 108.65},
+        {x = -3039.46, y = 585.99,   z = 7.94},
+        {x = -3242.34, y = 1001.7,   z = 12.87},
+        {x = 547.44,   y = 2671.2,   z = 42.19},
+        {x = 1961.43,  y = 3740.94,  z = 32.37},
+        {x = 2678.9,   y = 3280.68,  z = 55.27},
+        {x = 1729.34,  y = 6414.49,  z = 35.1},
+        {x = 25.95,    y = -1345.46, z = 29.51},
+        {x = 1136.14,  y = -982.3,   z = 46.46},
+        {x = -1223.19, y = -907.07,  z = 10.35},
+        {x = -1487.2,  y = -379.53,  z = 40.19},
+        {x = -2968.16, y = 390.43,   z = 15.09},
+        {x = 1166.02,  y = 2708.99,  z = 38.19},
+        {x = 1392.42,  y = 3604.59,  z = 35.0},
+        {x = -48.5,    y = -1757.58, z = 29.46},
+        {x = 1163.26,  y = -324.1,   z = 69.27},
+        {x = -707.67,  y = -914.72,  z = 19.28},
+        {x = -1820.71, y = 792.42,   z = 138.19},
+        {x = 1698.44,  y = 4924.65,  z = 42.1},
+    },
+    Blips = {
+        Activer = true,
+        Sprite = 59,
+        Color = 2,
+        Scale = 0.6,
+        Name = "Supérette",
+    },
+}
---
+++ server/server.lua
@@ -0,0 +1,27 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterNetEvent('superettes:achat_item')
+AddEventHandler('superettes:achat_item', function(name, price, liquide)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local xMoneyL = xPlayer.getMoney()
+    local xMoneyB = xPlayer.getAccount('bank').money
+    if liquide then
+        if xMoneyL >= price then
+            xPlayer.removeMoney(price)
+            xPlayer.addInventoryItem(name, 1)
+            TriggerClientEvent('esx:showNotification', source, "Vous avez payé ~g~"..price.."$")
+        else
+            TriggerClientEvent('esx:showNotification', source, "~r~Vous n'avez pas assez d'argent")
+        end
+    else
+        if xMoneyB >= price then
+            xPlayer.removeAccountMoney('bank', price)
+            xPlayer.addInventoryItem(name, 1)
+            TriggerClientEvent('esx:showNotification', source, "Vous avez payé ~b~"..price.."$")
+        else
+            TriggerClientEvent('esx:showNotification', source, "~r~Vous n'avez pas assez d'argent")
+        end
+    end
+end)
