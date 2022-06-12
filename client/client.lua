66ae0fb8aa191a7c920b5869223d240123bfacfb
wiizz <66723882+wiizzdev@users.noreply.github.com>
2022-06-12 18:34:08 +0200

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -9,10 +9,10 @@ Citizen.CreateThread(function()
 	end
 end)
 
-local mainMenu = RageUI.CreateMenu("", "Supérette")
-local nourriture = RageUI.CreateSubMenu(mainMenu, "", "Nourriture")
-local boissons = RageUI.CreateSubMenu(mainMenu, "", "Boissons")
-local divers = RageUI.CreateSubMenu(mainMenu, "", "Divers")
+local mainMenu = RageUI.CreateMenu("Supérette", "MENU")
+local nourriture = RageUI.CreateSubMenu(mainMenu, "Nourriture", "MENU")
+local boissons = RageUI.CreateSubMenu(mainMenu, "Boissons", "MENU")
+local divers = RageUI.CreateSubMenu(mainMenu, "Divers", "MENU")
 local open,Index = false,1
 
 mainMenu.Closed = function() open = false end
